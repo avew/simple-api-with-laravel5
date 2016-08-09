c8e2a9b192dd96b2ec234ef87e282f7b0c5a2a78
Asep  Rojali <aseprojali@gmail.com>
2016-08-09 23:16:12 +0700

    add route authenticated and refresh token

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+/**
+ * Created with love.
+ * User: aseprojali
+ * Date: 8/9/16
+ * Time: 8:38 PM
+ * Website : avew.github.io
+ */
+
+use App\Http\Controllers\ApiController;
+use App\Permission;
+use App\Role;
+use App\User;
+use Dingo\Api\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UsersController extends ApiController
+{
+
+
+    /**
+     * Get user logged in with token
+     *
+     * @return mixed
+     */
+    public function authenticated()
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if (!$user) {
+                return $this->response->errorNotFound('User not found');
+            }
+
+        } catch (JWTException $ex) {
+            return $this->response->error('Something went wrong');
+        }
+        return \Response::json([
+            'data' => [
+                'email' => $user->email,
+                'registered_at' => $user->created_at->toDateTimeString()
+            ]
+        ]);
+    }
+
+
+    /**
+     * Refresh the token and send back to client
+     *
+     */
+    public function refreshToken()
+    {
+
+        $token = JWTAuth::getToken();
+        if (!$token) {
+            return $this->response->errorUnauthorized('Token is invalid');
+        }
+        try {
+            $refreshedToken = JWTAuth::refresh($token);
+
+        } catch (JWTException $ex) {
+            $this->response->error('Something went wrong');
+        }
+
+        return $this->response->array(compact('refreshedToken'));
+    }
+
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,17 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-    
+
     $api->get('hello', 'App\Http\Controllers\Api\HomeController@index');
 
     $api->post('authenticate', 'App\Http\Controllers\Auth\ApiAuthController@authenticate');
 
+});
+
+$api->version('v1', ['middleware' => 'api.auth'], function ($api) {
+
+    $api->get('authenticated', 'App\Http\Controllers\Api\UsersController@authenticated');
+
+    $api->get('token', 'App\Http\Controllers\Api\UsersController@refreshToken');
+    
 });
